d3045b2e79f461c6a6cfc22958f760fd6e30fb77
ll799268 <1010023095@qq.com>
2021-12-31 16:35:53 +0800

    demo

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <p>213</p>
+  <p>{{ data1 }}</p>
 </template>
 
-<script setup>
-  
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const data1 = ref(1)
+    return {
+      data1: data1.value
+    }
+  }
+}
 </script>
 
 <style>
